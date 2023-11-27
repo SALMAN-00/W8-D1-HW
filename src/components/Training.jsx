3d7f3104e07894706d5981a0518cb011e3365e9c
iplmr <45397969+iplmr@users.noreply.github.com>
2023-11-27 09:30:39 +0300

    HW

---
+++ src/components/Training.jsx
@@ -0,0 +1,274 @@
+function Training() {
+  return (
+    <>
+      <div className="grid  grid-rows-6 grid-flow-col gap-3 w-full h-full bg-white ">
+    
+
+
+
+        <div className="row-span-1  bg-red-200 h-[78vh]"m  >
+          <div className="grid grid-rows-4 justify-items-start place-items-center ml-20 ">
+
+          <div className="row-span-1 mt-12">
+          <p className="text-white font-mono">Fresh<span className="font-bold font-mono">Goods</span></p>
+          </div>
+
+          <div className="row-span1 ">
+            <p className="text-white font-thin text-4xl">Concisely <br></br>
+            describe your<br></br>product or service</p>
+            </div>
+
+            <div className="row-span-1">
+              <p className="text-white text-xs ">No need get clever. Tell people exactly what you're offering,then <br />use this to communicate your key value proposition</p>
+            </div>
+
+            <div className="row-span-1 mb-20">
+              <button className="bg-yellow-500 rounded-sm px-4 py-1 text-xs font-bold">ORDER NOW</button>
+            </div>
+
+        </div>
+        </div>
+
+
+
+
+
+
+
+
+        
+          
+    
+
+        <div className="row-span-1  bg-green-200 justify-center h-[78vh]">
+<div className="grid grid-rows-2 justify-center">
+  <div className="row-span-1 ">
+<h1 className="text-3xl">Here are some of the benefits<br></br>of your offer</h1>
+<p className="text-xs mt-5">Explain what makes your product or service great. Talk about features in a way<br></br>that highlights the real value people get out of them.</p>
+  </div>
+<div className="row-span-2 grid grid-cols-3 justify-center h-[10vh]" >
+  <div className="col-span-1 grid grid-rows-3 justify-evenly" >
+<div className="row-span-1">
+  <img src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/3a18a275-freshgoods-benefit1-icon_1000000000000000000028.png"  />
+</div>
+<div className="row-span-2 ">
+  <h1 className="font-bold">Benefit 1</h1>
+</div>
+<div className="row-span-3">
+  <p className="text-sm">for example, restaurants and<br></br> bakeries could outline the<br></br> health benefits of
+  their all<br></br>  natural ingredients</p>
+</div>
+  </div>
+
+
+  <div className="col-span-2 grid grid-rows-3 justify-evenly">
+
+<div className="row-span-1">
+  <img src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/b41bc87d-freshgoods-benefit2-icon_1000000000000000000028.png" alt="" />
+</div>
+<div className="row-span-2">
+  <h1 className="bold">Benefit 2</h1>
+</div>
+<div className="row-span-3">
+  <p className="text-xs">Florists and other small retailers<br></br>
+   might use this space to describe<br></br>  how their products make<br></br>
+ delightful,one-of-a-kind gifts.  </p>
+</div>
+
+  </div>
+
+
+
+
+
+
+  <div className="col-span-3 grid grid-rows-3 justify-evenly">
+    <div className="row-span-1">
+      <img src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/6622ad60-freshgoods-benefit3-icon_1000000000000000000028.png" alt="" />
+    </div>
+    <div className="row-span-2">
+      <h1 className="font-bold">Benefit 3</h1>
+    </div>
+    <div className="row-span-3">
+      <p className="text-xs">You could also add button to<br></br>
+      this section and share links to<br></br>
+       other products or availbale<br></br>
+         delivery methods.</p>
+    </div>
+  </div>
+
+
+
+</div>
+</div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div className="row-span-1  bg-zinc-500 grid grid-rows-3 h-[78vh]" >
+<div className="row-span-1">
+<p className="text-xs">What our customers say</p>
+</div>
+<div className="row-span-2">
+  <h1 className="text-yellow-500 text-3xl">،،</h1>
+<h1>share a real testimonail that hits some of<br></br>
+  your benefits (but isn't too sales-y)</h1>
+</div>
+<div className="row-span-3">
+  <img src="https://lying-move-262.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F69d3b502-87ee-4689-8097-3171d8f07c07%2F587152f7-471a-4914-8660-580bfcfd35ca%2FUntitled.png?table=block&id=41befdbe-5603-4b97-9d5f-fce887f451de&spaceId=69d3b502-87ee-4689-8097-3171d8f07c07&width=200&userId=&cache=v2"  />
+  <p className="text-xs">REAL NAME<br></br>  location</p>
+  <img src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/adb06479-stars.svg" alt="" />
+
+</div>
+        </div>
+
+
+
+        <div className="row-span-1  border-l-orange-400 grid grid-cols-3 h-[78vh]">
+<div className="col-span-1 grid grid-rows-3">
+<div className="row-span-1">
+  <img src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/6765ae81-freshgoods-feature1_10go0b20go09e00000001o.jpg" alt="" />
+</div>
+<div className="row-span-2">
+  <h1 className="font-bold">Feature 1</h1>
+</div>
+<div className="row-span-3">
+  <p className="text-xs">Talk about some of the details of<br></br>
+   your offer with a focus on the<br></br>
+     value people get back.
+  </p>
+</div>
+
+
+
+
+
+
+</div>
+
+<div className="col-span-2 grid grid-rows-3">
+<div className="row-span-1">
+  <img src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/bf92a2f6-freshgoods-feature2_10go0ku0go09e00009e01o.jpg" alt="" />
+</div>
+<div className="row-span-2">
+<h1 className="font-bold">Feature 2</h1>
+</div>
+<div className="row-span-3">
+  <p className="text-xs"> Is there a pain point that your<br></br>
+  product or service resolves?Tell<br></br>
+      visitors about it here</p>
+</div>
+</div>
+
+
+<div className="col-span-3 grid grid-rows-3">
+<div className="row-span-1">
+  <img src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/f55dfcfa-freshgoods-feature3_10go0b40go09e00000601o.jpeg" alt="" />
+</div>
+<div className="row-span-2">
+<h1 className="font-bold">Feature 3</h1>
+
+</div>
+<div className="row-span-3">
+  <p className="text-xs">Alternatively,you could use this<br></br>
+    section to address some<br></br>
+    frequcntly asked questions</p>
+</div>
+</div>
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div className="row-span-1  border-b-indigo-500 grid grid-cols-2 h-[78vh]">
+<div className="col-span-1">
+<img src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/freshgoods/ef9d9b9f-freshgoods-about_100000000000000000001o.jpg" alt="" />
+</div>
+
+<div className="col-span-2">
+<h1 className="text-3-lg">Some more<br></br>
+information about<br></br>
+your business</h1>
+<p className="text-sm">Share a little about yourself as a business owner,or maybe<br></br>
+decribe what makes your product or service unique.Give<br></br>
+visitors one more reason to care about your offer and want<br></br>
+to buy from you.</p>
+</div>
+
+        </div>
+
+
+
+
+
+
+
+        <div className="row-span-1  border-b-orange-700 grid grid-rows-4 h-[78vh]">
+          <div className="row-span-1">
+<h1 className="text-4lg">End with one<br></br>
+final call to action</h1>
+          </div>
+
+          <div className="row-span-1">
+<p className="text-xs">this is your last shot converting someone.Remaind them about the<br></br>
+   benefits of your offer and encourage them to act now.</p>
+</div>
+
+<div className="row-span-1">
+<button className="bg-yellow-500 rounded-sm px-4 py-1 text-xs font-bold">ORDER NOW</button>
+
+</div>
+<div className="row-span-1 grid grid-cols-2">
+<div className="col-span-1"> 
+<p className="text-white">Fresh<b>Goods</b></p>
+</div>
+<div className="col-span-1">
+<p className="text-white text-xs">© 2020 Insert Name Here. All rights reserved.</p>
+</div>
+  
+
+</div>
+
+
+
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Training;
